7d018b3be70540bad234d7661f8a6cdb5a16894b
Semiranov <semiranov@gmail.com>
2022-11-29 20:48:56 +0300

    add Seminar1 task1

---
+++ Лекции/Lesson2.py
@@ -0,0 +1,20 @@
+# colors = ['red', 'green', 'blue']
+# data = open('file.txt', 'a')
+# #data.write(colors) #разделителей не будет
+# data.write('\nLINE 2\n')
+# data.write('\nLINE 3\n')
+# data.close()
+
+# path = 'file.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+
+def f(x):
+    if x == 1:
+        return 'Целое'
+    elif x == 2.3:
+        return 23
+    else:
+        return 
