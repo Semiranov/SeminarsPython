b767fa65e37d9a3fe605b60290e1e6982eaf1ee2
Semiranov <semiranov@gmail.com>
2022-11-29 11:07:58 +0300

    add Seminar2 HW task 1

---
+++ 2/HW/task1.py
@@ -0,0 +1,15 @@
+# 1.	Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+
+# num = input('Введите вещественное число ')
+# sum = 0
+# for i in range(len(num)):
+#     if num[i] != '.':
+#         sum += int(num[i])
+# print(sum)
+
+num = input('Введите вещественное число ')
+sum = 0
+for i in range(len(num)):
+    if num[i].isdigit():
+        sum += int(num[i])
+print(sum)
