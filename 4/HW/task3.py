ebc040934d1581ad8aede46d445b814b1e35927d
Semiranov <semiranov@gmail.com>
2022-12-07 00:18:59 +0300

    add Seminar4 HW task3

--- 4/HW/task3.py
+++ 4/HW/task3.py
@@ -1,23 +1,19 @@
 # 3.	Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов исходной последовательности.
 # [1, 2, 2, 3, 4]  -> [1, 3, 4]
-import math
+
 import random
 
-# num = int(input('введите длинну желаемого списка: '))
-# sp = []
-# for i in range(num):
-#     sp.append(random.randint(0,10))
-# print(sp)
-sp = [10, 3, 1, 1, 10]
+num = int(input('введите длинну желаемого списка: '))
+sp = []
+for i in range(num):
+    sp.append(random.randint(0,10))
+print(sp)
+
 sp2 = []
-a = sp.pop(0)
 for i in range(len(sp)):
-    if a not in sp:
-        sp2.append(a)
-        a = sp.pop(0)
-    else:
-        a = sp.pop(0)
+    if sp.count(sp[i]) == 1:
+        sp2.append(sp[i])
 print(sp2)
 
 
