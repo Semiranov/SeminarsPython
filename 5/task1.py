09e13b54a12d8312eb0341c7972131678a51731a
Semiranov <semiranov@gmail.com>
2022-12-06 21:55:54 +0300

    add HW5 alltasks

---
+++ 5/task1.py
@@ -0,0 +1,12 @@
+# 1.	В файле находится N натуральных чисел, записанных через пробел.
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
+
+path = 'file.txt'
+data = open(path, 'r')
+a = (data.read().split())
+a = list(map(int, a))
+print(a)
+data.close()
+for i in range(1, len(a)):
+    if a[i] != a[i - 1] + 1:
+        print(a[i - 1] + 1)
